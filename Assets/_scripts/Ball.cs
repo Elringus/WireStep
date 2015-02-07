381c311ac51e2b241b6a1e1c7d993de0714f22c1
Elringus <elringus@yandex.ru>
2015-02-08 01:34:57 +0300

    initial commit

---
+++ Assets/_scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private const float LINE_WIDTH = .05f;
+
+	private DrawManager drawManager;
+
+	private void Awake ()
+	{
+		drawManager = FindObjectOfType<DrawManager>();
+	}
+
+	private void Update ()
+	{
+		if (drawManager.LineEnd.HasValue && drawManager.LineStart.HasValue)
+			print(LineContainsPoint(new Vector2[2] { drawManager.LineStart.Value, drawManager.LineEnd.Value }, Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+	}
+
+	private bool LineContainsPoint (Vector2[] line, Vector2 point)
+	{
+		if (Vector3.Cross(line[1] - line[0], point - line[0]).sqrMagnitude > LINE_WIDTH) return false;
+
+		float dot = Vector2.Dot(line[1] - line[0], point - line[0]);
+		if (dot < 0) return false;
+
+		if (dot > Mathf.Pow(Vector2.Distance(line[0], line[1]), 2)) return false;
+
+		return true;
+	}
+}
---
+++ Assets/_scripts/DrawManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class DrawManager : MonoBehaviour
+{
+	public Vector2? LineStart;
+	public Vector2? LineEnd;
+
+	private LineRenderer line;
+
+	private void Awake () 
+	{
+		line = FindObjectOfType<LineRenderer>();
+	}
+
+	private void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+			LineStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		if (Input.GetMouseButtonUp(0)) 
+		{
+			LineEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			DrawLine(); 
+		}
+	}
+
+	private void DrawLine ()
+	{
+		if (!LineStart.HasValue || !LineEnd.HasValue) return;
+
+		line.SetPosition(0, LineStart.Value);
+		line.SetPosition(1, LineEnd.Value);
+
+		//LineStart = null;
+		//LineEnd = null;
+	}
+}
